8acc01ee9634dfe5d6726862c54593b8323e6e3c
Gabi Dobocan <gabi@telepat.io>
2021-05-09 06:52:37 +0300

    Fix export issue

--- media-support/src/constants.js
+++ media-support/src/constants.js
@@ -1,17 +1,16 @@
-export default {
-  FAMILY_TYPE: {
-    STREAMING_DEVICE: 'streaming-device',
-    TV: 'tv',
-    HANDHELD: 'handheld',
-    BROWSER: 'browser',
-  },
-  FEATURE_TYPE: {
-    VIDEO_CODEC: 'video',
-    AUDIO_CODEC: 'audio',
-    MEDIA_CONTAINER: 'container',
-    STREAMING_TECH: 'streaming',
-    DRM_TECH: 'drm',
-    HDR_TECH: 'hdr',
-    CAPTION_FORMAT: 'subtitle',
-  },
+export const FAMILY_TYPE = {
+  STREAMING_DEVICE: 'streaming-device',
+  TV: 'tv',
+  HANDHELD: 'handheld',
+  BROWSER: 'browser',
+};
+  
+export const FEATURE_TYPE= {
+  VIDEO_CODEC: 'video',
+  AUDIO_CODEC: 'audio',
+  MEDIA_CONTAINER: 'container',
+  STREAMING_TECH: 'streaming',
+  DRM_TECH: 'drm',
+  HDR_TECH: 'hdr',
+  CAPTION_FORMAT: 'subtitle',
 };
