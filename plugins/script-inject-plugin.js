77c598abf79ae519889154f224ec78ef85097d01
Gabi Dobocan <gabi@telepat.io>
2021-05-12 04:59:12 +0300

    Update GA tag

--- plugins/script-inject-plugin.js
+++ plugins/script-inject-plugin.js
@@ -4,8 +4,19 @@ module.exports = () => ({
     return {
       headTags: [
         `
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-2YDLGY5BJY"></script>
-          <script src="/scripts/ga.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114384176-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-114384176-1', {
+    'linker': {
+      'domains': ['veeplay.com', 'docs.veeplay.com', 'panel.veeplay.com']
+    }
+  });
+</script>
         `,
       ],
     };
